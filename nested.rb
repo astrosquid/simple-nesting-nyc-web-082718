4a1ac70f175b4bb457e9b04367a35d12cb4deeab
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 05:32:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +82,10 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
 end
 
 def changing_alan
@@ -103,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = alan_kay
 end
 
 def adding_to_dennis
@@ -122,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][languages] << "Assembly"
 end
