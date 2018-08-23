7cb4fbcb0a07660de6ce7ea7e6b7f60d445e25b7
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 05:34:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -110,6 +110,8 @@ def changing_alan
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = alan_kay
+  
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -129,4 +131,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][languages] << "Assembly"
+  programmer_hash
 end
